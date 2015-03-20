bfe087a607e4d8680d0e691d0e28de0f7cc7bd6b
khristenkovladimir <khristenko.vladimir1986@gmail.com>
2015-03-20 15:07:03 +0200

    new print

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.print("hello");// write your code here
+        System.out.println(" world");
     }
 }
