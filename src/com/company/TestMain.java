11b3247094b8f191a65faf968a11023132425bdc
khristenkovladimir <khristenko.vladimir1986@gmail.com>
2015-03-20 15:14:59 +0200

    new file

---
+++ src/com/company/TestMain.java
@@ -0,0 +1,10 @@
+package com.company;
+
+/**
+ * Created by admin on 20.03.2015.
+ */
+public class TestMain {
+    public static void main(String[] args) {
+        System.out.println("it's working, mother fakers");
+    }
+}
